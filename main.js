6f0d2de58bea48277b073d6b65705c5f4c4403f0
Mirziyod <mirsaidovmirziyod1507@gmail.com>
2023-05-06 20:50:25 +0500

    I think all work is done

---
+++ main.js
@@ -0,0 +1,8 @@
+let form = [
+    {firstName: prompt('Enter your name')},
+    {surName: prompt('Enter your surname')},
+    {sign: prompt('Enter your age')},
+    {location: prompt('Please tell me where are you from')}
+]
+
+console.log(form);
