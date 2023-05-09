ceb14fb167396561962e16fd163cb313ba57d117
Mirziyod <mirsaidovmirziyod1507@gmail.com>
2023-05-09 15:34:02 +0500

    some bugs fixed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let form = [
-    {firstName: prompt('Enter your name')},
-    {surName: prompt('Enter your surname')},
-    {sign: prompt('Enter your age')},
-    {location: prompt('Please tell me where are you from')}
-]
+let form = {
+  firstName: prompt("Enter your name"),
+  surName: prompt("Enter your surname"),
+  Age: prompt("Enter your age"),
+  location: prompt("Please tell me where are you from"),
+};
 
 console.log(form);
